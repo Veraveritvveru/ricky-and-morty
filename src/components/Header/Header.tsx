fd164c0b2219a59b7149589351220fec0f66872c
Vera Krumpan <filimonovavera96@gmail.com>
2025-11-05 11:04:04 +0300

    refactor: header component from class to function

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import styles from './Header.module.scss';
 import logo from '../../assets/logo.svg';
@@ -9,18 +9,22 @@ interface HeaderProps {
   onSearchSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-export default class Header extends Component<HeaderProps> {
-  public override render() {
-    return (
-      <header className={styles.header}>
-        <img src={logo} alt="Rick and Morty logo" />
-        <SearchForm
-          inputValue={this.props.inputValue}
-          onInputChange={this.props.onInputChange}
-          onSearchSubmit={this.props.onSearchSubmit}
-        />
-        <div>ThemeSwitch</div>
-      </header>
-    );
-  }
-}
+const Header: React.FC<HeaderProps> = ({
+  inputValue,
+  onInputChange,
+  onSearchSubmit,
+}) => {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Rick and Morty logo" />
+      <SearchForm
+        inputValue={inputValue}
+        onInputChange={onInputChange}
+        onSearchSubmit={onSearchSubmit}
+      />
+      <div>ThemeSwitch</div>
+    </header>
+  );
+};
+
+export default Header;
